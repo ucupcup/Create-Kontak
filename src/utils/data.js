42c4e822116460f23582991d2ce785288941f18d
Yusuf Ilham Agung <yusufilhamagung@gmail.com>
2024-10-10 08:17:32 +0700

    Create Kontak

---
+++ src/utils/data.js
@@ -0,0 +1,24 @@
+const getData = () => {
+    return [
+        {
+            id: 1,
+            name: 'Dimas Saputra',
+            tag: 'Dimmy',
+            imageUrl: '/images/dimasmds.jpeg',
+        },
+        {
+            id: 2,
+            name: 'Arif Item',
+            tag: 'Item',
+            imageUrl: '/images/arifaizin.jpeg',
+        },
+        {
+            id: 3,
+            name: 'Rahmat',
+            tag: 'Mateo',
+            imageUrl: '/images/rfajri27.jpeg',
+        }
+    ];
+}
+
+export { getData };
