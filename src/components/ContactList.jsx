954f8294bccd6c3778d1415d031ef184b24e8883
Yusuf Ilham Agung <yusufilhamagung@gmail.com>
2024-10-15 23:04:34 +0700

    Add Delete Button

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,16 +1,30 @@
-import React from "react";
+// import React from "react";
 import ContactItem from "./ContactItem";
+// import PropTypes from 'prop-types';
 
-function ContactList({ contacts }) {
+function ContactList({ contacts, onDelete }) {
     return (
         <div className="contact-list">
             {
                 contacts.map((contact) => (
-                    <ContactItem key={contact.id} {...contact} />
+                    <ContactItem
+                    key={contact.id}
+                    id={contact.id}
+                    onDelete={onDelete}
+                    {...contact} />
                 ))
             }
         </div>
     );
 }
+
+// ContactList.propTypes = {
+//     contacts: PropTypes.arrayOf(PropTypes.shape({
+//         imageUrl: PropTypes.string.isRequired,
+//         name: PropTypes.string.isRequired,
+//         tag: PropTypes.string.isRequired
+//     })).isRequired,
+// };
+
 
 export default ContactList;
